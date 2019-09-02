0c936e466999a1fe0d4f2bc08e28d232b87fdfc5
Kingsley <448409934@qq.com>
2019-09-02 08:09:38 +0800

    logs to understand

--- redux-master/examples/todos/src/containers/VisibleTodoList.js
+++ redux-master/examples/todos/src/containers/VisibleTodoList.js
@@ -16,9 +16,12 @@ const getVisibleTodos = (todos, filter) => {
   }
 }
 
-const mapStateToProps = state => ({
-  todos: getVisibleTodos(state.todos, state.visibilityFilter)
-})
+const mapStateToProps = state => {
+  console.log('visibilityFilter-state', state)
+  return {
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   toggleTodo: id => dispatch(toggleTodo(id))
--- redux-master/examples/todos/src/reducers/visibilityFilter.js
+++ redux-master/examples/todos/src/reducers/visibilityFilter.js
@@ -1,6 +1,8 @@
 import { VisibilityFilters } from '../actions'
 
 const visibilityFilter = (state = VisibilityFilters.SHOW_ALL, action) => {
+  console.log('reducer-action', action)
+  console.log('reducer-state', state)
   switch (action.type) {
     case 'SET_VISIBILITY_FILTER':
       return action.filter
